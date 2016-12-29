3c70b37ecf35082288df3d0e123f15f72dc2469f
Dave Wood <dave@cerebralgardens.com>
2016-12-29 16:19:55 -0500

    Convert to current Swift syntax

--- JotTouchExample/JotTouchExample/Numbers.swift
+++ JotTouchExample/JotTouchExample/Numbers.swift
@@ -21,7 +21,7 @@ extension Float {
 	}
 	
 	/// Returns a random float from min to max
-	public static func randomWithin(min min: Float, max: Float) -> Float {
+	public static func randomWithin(min: Float, max: Float) -> Float {
 		return Float.random() * (max - min) + min
 	}
 	
@@ -30,7 +30,7 @@ extension Float {
 	}
 	
 	/// Assuming this number is from 0 to 1, linearly interpolate from and to
-	func lerpAsDelta(from from:Float, to:Float) -> Float {
+	func lerpAsDelta(from:Float, to:Float) -> Float {
 		return from + (to - from) * self
 	}
 	
@@ -40,13 +40,13 @@ extension Float {
 	}
 	
 	/// Transform this value as percentage of an old range to a new range
-	func mapFrom(mapFromStart:Float, _ mapFromEnd:Float, to mapToStart:Float, _ mapToEnd:Float, clamped:Bool = false) -> Float {
+	func mapFrom(_ mapFromStart:Float, _ mapFromEnd:Float, to mapToStart:Float, _ mapToEnd:Float, clamped:Bool = false) -> Float {
 		var from = (self - mapFromStart) / (mapFromEnd - mapFromStart)
 		if clamped {
 			from = from.clamp(min: mapFromStart, max: mapFromEnd)
 		}
 		
-		var to = from.lerpAsDelta(from: mapToStart, to: mapToEnd)
+        var to = from.lerpAsDelta(from: mapToStart, to: mapToEnd)
 		if clamped {
 			to = to.clamp(min: mapToStart, max: mapToEnd)
 		}
@@ -55,11 +55,11 @@ extension Float {
 	}
 	
 	/// Similar to lerpAsDelta
-	func mapAsDeltaTo(mapToStart:Float, _ mapToEnd:Float) -> Float {
+	func mapAsDeltaTo(_ mapToStart:Float, _ mapToEnd:Float) -> Float {
 		return self.mapFrom(0, 1, to: mapToStart, mapToEnd)
 	}
 	
-	func clamp(min min:Float, max:Float) -> Float {
+	func clamp(min:Float, max:Float) -> Float {
 		let from = min < max ? min : max
 		let to = min > max ? min : max
 		return self < from ? from : (self > to ? to : self)
@@ -71,55 +71,55 @@ extension Float {
 }
 
 extension CGPoint {
-	@inline(__always) func distanceTo(other:CGPoint) -> CGFloat {
+	@inline(__always) func distanceTo(_ other:CGPoint) -> CGFloat {
 		let dx = other.x - self.x
 		let dy = other.y - self.y
 		return sqrt(dx * dx + dy * dy)
 	}
 	
-	func mapFrom(mapFrom:CGRect, to mapTo:CGRect, clamped:Bool = false) -> CGPoint {
-		let dx = self.x.mapFrom(CGRectGetMinX(mapFrom), CGRectGetMaxX(mapFrom), to: CGRectGetMinX(mapTo), CGRectGetMaxX(mapTo), clamped: clamped)
-		let dy = self.y.mapFrom(CGRectGetMinY(mapFrom), CGRectGetMaxY(mapFrom), to: CGRectGetMinY(mapTo), CGRectGetMaxY(mapTo), clamped: clamped)
+	func mapFrom(_ mapFrom:CGRect, to mapTo:CGRect, clamped:Bool = false) -> CGPoint {
+		let dx = self.x.mapFrom(mapFrom.minX, mapFrom.maxX, to: mapTo.minX, mapTo.maxX, clamped: clamped)
+		let dy = self.y.mapFrom(mapFrom.minY, mapFrom.maxY, to: mapTo.minY, mapTo.maxY, clamped: clamped)
 		
-		return CGPointMake(dx, dy)
+		return CGPoint(x: dx, y: dy)
 	}
 	
-	func mapAsDeltaTo(mapTo:CGRect, clamped:Bool = false) -> CGPoint {
-		return self.mapFrom(CGRectMake(0, 0, 1, 1), to: mapTo, clamped: clamped)
+	func mapAsDeltaTo(_ mapTo:CGRect, clamped:Bool = false) -> CGPoint {
+		return self.mapFrom(CGRect(x: 0, y: 0, width: 1, height: 1), to: mapTo, clamped: clamped)
 	}
 	
 	func clamp(bounds:CGRect) -> CGPoint {
-		let dx = self.x.clamp(min: CGRectGetMinX(bounds), max: CGRectGetMaxX(bounds))
-		let dy = self.y.clamp(min: CGRectGetMinY(bounds), max: CGRectGetMaxY(bounds))
+		let dx = self.x.clamp(min: bounds.minX, max: bounds.maxX)
+		let dy = self.y.clamp(min: bounds.minY, max: bounds.maxY)
 		
-		return CGPointMake(dx, dy)
+		return CGPoint(x: dx, y: dy)
 	}
 	
-	func withinRangeOf(other:CGPoint, range:CGFloat) -> Bool {
+	func withinRangeOf(_ other:CGPoint, range:CGFloat) -> Bool {
 		return self.distanceTo(other) <= range
 	}
 	
-	func lerpTo(other:CGPoint, delta:CGFloat) -> CGPoint {
+	func lerpTo(_ other:CGPoint, delta:CGFloat) -> CGPoint {
 		let x = self.x.lerpTo(other.x, delta: delta)
 		let y = self.y.lerpTo(other.y, delta: delta)
 		
-		return CGPointMake(x, y)
+		return CGPoint(x: x, y: y)
 	}
 	
 	/// Add x and y of other to our x and y
-	func offset(other:CGPoint) -> CGPoint {
-		return CGPointMake(x + other.x, y + other.y)
+	func offset(_ other:CGPoint) -> CGPoint {
+		return CGPoint(x: x + other.x, y: y + other.y)
 	}
 	
 	/// Multiply our x and y by this new value
-	func multiply(value:CGFloat) -> CGPoint {
-		return CGPointMake(x * value, y * value)
+	func multiply(_ value:CGFloat) -> CGPoint {
+		return CGPoint(x: x * value, y: y * value)
 	}
 }
 
 extension CGSize {
 	func toPoint() -> CGPoint {
-		return CGPointMake(self.width, self.height)
+		return CGPoint(x: self.width, y: self.height)
 	}
 }
 
@@ -129,28 +129,28 @@ extension CGFloat {
 		return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
 	}
 	
-	public static func randomWithin(min min: Float, max: Float) -> CGFloat {
+	public static func randomWithin(min: Float, max: Float) -> CGFloat {
 		return CGFloat(Float.random() * (max - min) + min)
 	}
 	
-	func withinRangeOf(other:CGFloat, range:CGFloat) -> Bool {
+	func withinRangeOf(_ other:CGFloat, range:CGFloat) -> Bool {
 		return fabs(self - other) <= range
 	}
 	
-	func lerpAsDelta(from from:CGFloat, to:CGFloat) -> CGFloat {
+	func lerpAsDelta(from:CGFloat, to:CGFloat) -> CGFloat {
 		return from + (to - from) * self
 	}
 	
-	func lerpAsDelta(from from:CGPoint, to:CGPoint) -> CGPoint {
+	func lerpAsDelta(from:CGPoint, to:CGPoint) -> CGPoint {
 		return from.lerpTo(to, delta: self)
 	}
 	
-	func lerpTo(other:CGFloat, delta:CGFloat) -> CGFloat {
+	func lerpTo(_ other:CGFloat, delta:CGFloat) -> CGFloat {
 		return self + (other - self) * delta
 	}
 	
-	func mapFrom(mapFromStart:CGFloat, _ mapFromEnd:CGFloat, to mapToStart:CGFloat, _ mapToEnd:CGFloat, clamped:Bool = false) -> CGFloat {
-		var from = (self - mapFromStart) / (mapFromEnd - mapFromStart)
+	func mapFrom(_ mapFromStart:CGFloat, _ mapFromEnd:CGFloat, to mapToStart:CGFloat, _ mapToEnd:CGFloat, clamped:Bool = false) -> CGFloat {
+        var from: CGFloat = (self - mapFromStart) / (mapFromEnd - mapFromStart)
 		if clamped {
 			from = from.clamp(min: mapFromStart, max: mapFromEnd)
 		}
@@ -163,11 +163,11 @@ extension CGFloat {
 		return to
 	}
 	
-	func mapAsDeltaTo(mapToStart:CGFloat, _ mapToEnd:CGFloat) -> CGFloat {
+	func mapAsDeltaTo(_ mapToStart:CGFloat, _ mapToEnd:CGFloat) -> CGFloat {
 		return self.mapFrom(0, 1, to: mapToStart, mapToEnd)
 	}
 	
-	func clamp(min min:CGFloat, max:CGFloat) -> CGFloat {
+	func clamp(min:CGFloat, max:CGFloat) -> CGFloat {
 		let from = min < max ? min : max
 		let to = min > max ? min : max
 		return self < from ? from : (self > to ? to : self)
@@ -185,22 +185,22 @@ extension CGFloat {
 extension CGRect {
 	/// Get midpoint of X and Y in rect
 	func getCenter() -> CGPoint {
-		return CGPointMake(CGRectGetMidX(self), CGRectGetMidY(self))
+		return CGPoint(x: self.midX, y: self.midY)
 	}
     
     /// Returns a point within the frame of the rectangle
     func randomInclusivePoint() -> CGPoint {
-        return CGPointMake(CGFloat.random().lerpAsDelta(from: CGRectGetMinX(self), to: CGRectGetMaxX(self)), CGFloat.random().lerpAsDelta(from: CGRectGetMinY(self), to: CGRectGetMaxY(self)))
+        return CGPoint(x: CGFloat.random().lerpAsDelta(from: self.minX, to: self.maxX), y: CGFloat.random().lerpAsDelta(from: self.minY, to: self.maxY))
     }
 }
 
 extension CGSize {
 	/// Returns a rect at 0,0 with this size and height
 	func makeRect() -> CGRect {
-		return CGRectMake(0, 0, self.width, self.height)
+		return CGRect(x: 0, y: 0, width: self.width, height: self.height)
 	}
 	
-	func multiply(num:CGFloat) -> CGSize {
+	func multiply(_ num:CGFloat) -> CGSize {
 		return CGSize(width: self.width * num, height: self.height * num)
 	}
 }
